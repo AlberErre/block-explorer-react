ad130ac231a2066ed9f10c83c3fb1a4bab13b488
AlberErre-experiments <a.ruizblugo@gmail.com>
2019-02-05 15:31:20 +0100

    fix CORS infura (downgrade to 1.0.0-beta.34)

--- app/src/App.js
+++ app/src/App.js
@@ -103,9 +103,7 @@ class App extends Component {
   };
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+
     return (
       <AragonApp publicUrl="/aragon-ui-assets/">
 
